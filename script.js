fbae1d77a8c2d714e7d446dfc0ff0a1508084d4a
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-18 14:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-	   
-    if(input_digit == ""){
-        return [];
+function letterCombinations(digits) {
+  const map = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+  
+  const result = [];
+  
+  if (digits.length === 0) {
+    return result;
+  }
+  
+  const backtrack = (combination, nextDigits) => {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const letters = map[nextDigits[0]];
+      for (let i = 0; i < letters.length; i++) {
+        backtrack(combination + letters[i], nextDigits.slice(1));
+      }
     }
-    let table = [ '0','1','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
-     
-    let res =[];
-    let que = [''];
-     
-    while(que.length>0){
-        let str = que[0];
-        que.shift();
-         
-        if(str.length == input_digit.length){
-            res.push(str); // if all digits are replaced with char push to result
-        } else{
-//             get the current number from the digits i.e if str.length = 2 , digits =123 s= 3
-            let s= Number(input_digit.charAt(str.length));
-            let value = table[s]; 
-			
-             
-            for(i=0;i<value.length;i++){
-                que.push(str+value.charAt(i));
-            }
-        }
-    }
-     
-    return res;
-     
+  };
+  
+  backtrack('', digits);
+  
+  return result.sort();
 }
+
+
+module.exports = letterCombinations;
